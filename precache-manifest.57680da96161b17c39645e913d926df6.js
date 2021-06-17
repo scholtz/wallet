489dba3b362ee34d0808d7a52da042413cbc4678
scholtz <ludko2@gmail.com>
2021-06-17 06:57:20 +0000

    Deploying to gh-pages from @ scholtz/wallet@6d9ef9fbbe7537c6dc9e0210e5c7d8020164730c 🚀

--- precache-manifest.57680da96161b17c39645e913d926df6.js
+++ precache-manifest.57680da96161b17c39645e913d926df6.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/apple-touch-icon.png"
   },
   {
-    "revision": "d8c1f7a4cfed8b6e8960",
+    "revision": "925802b08c75e44ad12d",
     "url": "/css/app.05b48347.css"
   },
   {
@@ -2164,12 +2164,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/primeicons.25954ab5.svg"
   },
   {
-    "revision": "4b0ba943b47ac0a9c0be3478639c19b4",
+    "revision": "57a574e4f002e3104c22bddf7e4fee3f",
     "url": "/index.html"
   },
   {
-    "revision": "d8c1f7a4cfed8b6e8960",
-    "url": "/js/app.599609f5.js"
+    "revision": "925802b08c75e44ad12d",
+    "url": "/js/app.8bc6e7f7.js"
   },
   {
     "revision": "3cb9e2b6747a5d2baa28",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a09fb941f1f929647272f4c651ce0179.js"
+  "/precache-manifest.57680da96161b17c39645e913d926df6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "wallet"});
