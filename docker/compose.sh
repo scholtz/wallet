cd011c50385f9b127f9a909b29986f054ada6681
Scholtz <ludko2@gmail.com>
2023-03-11 21:20:17 +0100

    cicd pipeline h3

---
+++ docker/compose.sh
@@ -0,0 +1,20 @@
+if [ "$ver" == "" ]; then
+ver=1.0.0
+fi
+
+echo "docker build -t \"scholtz2/a-wallet:$ver-main\" -f Dockerfile .."
+docker build -t "scholtz2/a-wallet:$ver-main" -f Dockerfile .. || error_code=$?
+if [ "$error_code" != "" ]; then
+echo "$error_code";
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push "scholtz2/a-wallet:$ver-main" || error_code=$?
+if [ "$error_code" != "" ]; then
+echo "$error_code";
+    echo "failed to push";
+	exit 1;
+fi
+
+echo "Image: scholtz2/a-wallet:$ver-main"
