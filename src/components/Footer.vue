6d2001730e8485c08977111ee914fc23ec1b4b4d
Kenny <kenmistry@outlook.com>
2023-03-28 21:05:40 +0800

    amended footer layout to use the container class and realign the footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,21 @@
 <template>
-  <footer>
-    <a
-      @click="prolong"
-      class="btn btn-xs btn-light float-end"
-      :style="'background:' + this.b"
-    >
-      {{ t }}
-    </a>
-    <div v-if="this.$store.state.wallet.isOpen" class="text-center">
-      {{ $t("footer.text") }}
+  <footer class="container-fluid pr-0 pl-0">
+    <div class="row">
+      <div class="col-1"></div>
+      <div v-if="this.$store.state.wallet.isOpen" class="col-10">
+        <p class="text-center m-0">
+          {{ $t("footer.text") + this.envStatus }}
+        </p>
+      </div>
+      <div class="col-1">
+        <a
+          @click="prolong"
+          class="btn btn-xs btn-light float-end"
+          :style="'background:' + this.b"
+        >
+          {{ t }}
+        </a>
+      </div>
     </div>
   </footer>
 </template>
