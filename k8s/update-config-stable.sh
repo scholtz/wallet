3d7c7a0f4f6e3d0e69abf48dd2b22d738af22f17
Scholtz <ludko2@gmail.com>
2024-06-05 12:18:21 +0200

    cicd deploy stable

--- k8s/update-config-stable.sh
+++ k8s/update-config-stable.sh
@@ -2,3 +2,4 @@ kubectl apply -f deployment-stable.yaml -n awallet
 kubectl delete configmap awallet-stable-conf -n awallet
 kubectl create configmap awallet-stable-conf --from-file=stable -n awallet
 kubectl rollout restart deployment/awallet-stable-deployment -n awallet
+kubectl rollout status deployment/awallet-stable-deployment -n awallet
--- k8s/update-config.sh
+++ k8s/update-config.sh
@@ -2,3 +2,4 @@ kubectl apply -f deployment-main.yaml -n awallet
 kubectl delete configmap awallet-main-conf -n awallet
 kubectl create configmap awallet-main-conf --from-file=conf -n awallet
 kubectl rollout restart deployment/awallet-web-main-landing-deployment -n awallet
+kubectl rollout status deployment/awallet-web-main-landing-deployment -n awallet
