7e55c5288dc7abe88606be6b3542ecb12221cdce
Ludovit Scholtz <ludko2@gmail.com>
2025-09-24 12:31:22 +0200

    feat: add projectId to Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "ere9gz",
   e2e: {
     baseUrl: "http://localhost:8083",
     supportFile: "cypress/support/e2e.ts",
